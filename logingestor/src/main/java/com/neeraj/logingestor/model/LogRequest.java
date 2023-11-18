76de15b026ca7e3d1ff3841a1313e002833a1858
dheeraj2897 <dheerajgupta2897@gmail.com>
2023-11-19 05:12:40 +0530

    add JsonProperty and update Lombok annotations to model

--- logingestor/src/main/java/com/neeraj/logingestor/model/LogRequest.java
+++ logingestor/src/main/java/com/neeraj/logingestor/model/LogRequest.java
@@ -1,5 +1,42 @@
 package com.neeraj.logingestor.model;
 
-public class LogRequest {
+import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.neeraj.logingestor.constant.LogConstant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LogRequest {
+	@JsonProperty("level")
+	private String level;
+	
+	@JsonProperty("message")
+	private String message;
+	
+	@JsonProperty("resourceId")
+	private String resourceId;
+	
+	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern = LogConstant.TIMESTAMP_FORMAT)
+	private LocalDateTime timestamp;
+	
+	@JsonProperty("traceId")
+	private String traceId;
+	
+	@JsonProperty("spanId")
+	private String spanId;
+	
+	@JsonProperty("commit")
+	private String commit;
+	
+	@JsonProperty("metadata")
+	private MetaData metadata;
 }
