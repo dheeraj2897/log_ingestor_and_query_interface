af9a8503081fd56c7a0f998ae8c1a4da68ef589c
dheeraj2897 <dheerajgupta2897@gmail.com>
2023-11-19 05:13:11 +0530

    update lombok annotations for entity

--- logingestor/src/main/java/com/neeraj/logingestor/entity/LogEntity.java
+++ logingestor/src/main/java/com/neeraj/logingestor/entity/LogEntity.java
@@ -1,5 +1,31 @@
 package com.neeraj.logingestor.entity;
 
-public class LogEntity {
+import java.time.LocalDateTime;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.neeraj.logingestor.model.MetaData;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Document(collection = "LOGS_DATA")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LogEntity {
+	@Id
+	private String documentId;
+	private String level;
+	private String message;
+	private String resourceId;
+	private LocalDateTime timestamp;
+	private String traceId;
+	private String spanId;
+	private String commit;
+	private MetaData metadata;
 }
---
+++ logingestor/src/main/java/com/neeraj/logingestor/entity/MetaData.java
@@ -0,0 +1,14 @@
+package com.neeraj.logingestor.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MetaData {
+	private String parentResourceId;
+}
